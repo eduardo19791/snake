c390690aa4885ca07192cf2d6ba8b63b1a2af131
eduardo19791 <antenassatellite@gmail.com>
2024-03-08 16:11:20 -0300

    ok

--- js/app.js
+++ js/app.js
@@ -2,11 +2,6 @@ const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 const size = 20
 
-<<<<<<< HEAD
-ctx.fillStyle = "#aaa"
-ctx.fillRect(280,280,size,size)
-=======
 const snake = [
       {x: 280, y:280}   
 ]
->>>>>>> d65fc15ba5957c7711112b4db9b36b0a73e5fc09
