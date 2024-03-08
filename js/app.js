d8ee449beda4b7e9f744a79ef844f19df574f1a5
eduardo19791 <antenassatellite@gmail.com>
2024-03-08 20:00:35 -0300

    ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 const size = 20
-const c1 = 'rgba(122,122,122,0.5)'
+const c1 = 'rgba(200,200,200,0.8)'
 const c2 = 'rgba(255,255,255,1.0)'
 
 const snake = [
@@ -15,3 +15,16 @@ const snake = [
     {x: 420, y: 280}
 ]
 
+const drawSnake = () => {
+    snake.forEach((e, k) => {
+        if(k == snake.length -1){
+            ctx.fillStyle = c2
+        }
+        else{
+            ctx.fillStyle = c1
+        }
+        ctx.fillRect(e.x, e.y, size, size)
+    })
+}
+
+drawSnake()
