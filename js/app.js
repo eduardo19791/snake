340c9f32ddd4ac14bdd5b85198194b11fdec62b6
eduardo19791 <antenassatellite@gmail.com>
2024-03-08 18:26:19 -0300

    ok

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 const size = 20
+const c1 = 'rgba(122,122,122,0.5)'
+const c2 = 'rgba(255,255,255,1.0)'
 
 const snake = [
-      {x: 280, y:280}   
+    {x: 280, y: 280},
+    {x: 300, y: 280},
+    {x: 320, y: 280},
+    {x: 340, y: 280},
+    {x: 360, y: 280},
+    {x: 380, y: 280},
+    {x: 400, y: 280},
+    {x: 420, y: 280}
 ]
+
